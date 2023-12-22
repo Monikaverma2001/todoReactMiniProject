580c221701db818ac6936b7e94f3cfe30945572f
Monikaverma2001 <monika8427084@gmail.com>
2023-12-22 15:58:01 +0530

    todo project completed

--- src/Component/Todo.jsx
+++ src/Component/Todo.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment } from "../Redux/Action";
 import "./Todo.css";
@@ -10,55 +10,69 @@ function Todo() {
   const [description, setDescription] = useState("description");
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.count.count);
- 
+
   // counter.map((result) => console.log(result.title + " " + result.description));
   return (
     <>
-      <h1>TODO LIST</h1>
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <br></br>
-      <input
-        type="text"
-        value={description}
+    <h1>TODO LIST</h1>
+      <div>
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="basic-addon1">
+              Title
+            </span>
+          </div>
+          <input
+            type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="basic-addon1">
+              Description
+            </span>
+          </div>
+          <input
+            type="text"
+            className="form-control"
+            value={description}
         onChange={(e) => setDescription(e.target.value)}
-      />
+          />
+        </div>
+      </div>
+
+      
       <br></br>
       <button
         onClick={() => {
           dispatch(increment(index, title, description));
           setIndex(Number(index) + Number(1));
-          
         }}
       >
         ADD TEXT
       </button>
 
-     
-
-      {JSON.parse(localStorage.getItem("list"))?.map((result) => (
-       
+      {counter?.map((result) => (
         <>
           <ul className="list">
-            <li>
-              <h4> {result["index"]}</h4>
+            <li className="mylist">
+             
               <h1>{result["title"]}</h1>
               <h4>{result["description"]}</h4>
-              <button
-        onClick={() => {
-          dispatch(decrement(result.index,result.title,result.description));
-          
-        }}
-      >
-        SUB TEXT
-      </button>
+              <button 
+                onClick={() => {
+                  dispatch(
+                    decrement(result.index, result.title, result.description)
+                  );
+                }}
+              >
+                SUB TEXT
+              </button>
             </li>
+            <hr/>
           </ul>
         </>
-      
       ))}
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './Component/Navbar.jsx'
+import About from './Component/About.jsx'
+import Contact from './Component/Contact.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<App/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
